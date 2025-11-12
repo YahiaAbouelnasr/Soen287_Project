c747326f2c6487f63c059e6e76760582d05a88e4
honeyow <anastasiatsermbis@gmail.com>
2025-11-12 15:19:41 -0500

    Changed the main folder to Front_end

--- Front_end/resource_management/main_page/resource_management.js
+++ Front_end/resource_management/main_page/resource_management.js
@@ -1,4 +1,4 @@
-import { allResources as staticResources } from "../main_page/staticData.js";
+import { allResources as staticResources } from "./staticData.js";
 
 let localResources = JSON.parse(localStorage.getItem("demoResources"));
 let demoResources = localResources && localResources.length > 0 ? localResources : staticResources;

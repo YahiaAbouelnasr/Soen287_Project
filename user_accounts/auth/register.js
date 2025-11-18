2e9992710191cc4cbbe2ee8eb2c067e2b8751355
YahiaAbouelnasr <yhelmy0401@gmail.com>
2025-11-18 11:52:08 -0500

    Implement user registration functionality
    This script handles user registration, validates email domains, and updates user profiles.

---
+++ user_accounts/auth/register.js
@@ -0,0 +1,43 @@
+import {auth} from "./firebase.js";
+import {
+    createUserWithEmailAndPassword, updateProfile
+}from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+
+
+const form = document.getElementById("registerForm");
+const nameInput = document.getElementById("name")
+const emailInput = document.getElementById("email");
+const passInput = document.getElementById("password");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const name = nameInput.value;
+    const email = emailInput.value;
+    const pass = passInput.value;
+
+    if(!email.endsWith("@live.concordia.ca")) {
+        alert("Email must be a concordia email");
+        emailInput.focus();
+        return
+    }
+
+    createUserWithEmailAndPassword(auth, email, pass) 
+    .then((uCredentials) => {
+        const user = uCredentials.user;
+
+        if(name) {
+            return updateProfile(user, {displayName : name}).then(() => user);
+        }
+
+    return user;
+    })
+    .then((user) => {
+        alert("Account Created, Please Log in.");
+        window.location.href = "../home.html";
+    }) 
+    .catch((err) => {
+            alert(err.message)
+    })
+})
+

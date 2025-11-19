ecb77a4d62fd66416c4f173805123090af2d5e75
YahiaAbouelnasr <yhelmy0401@gmail.com>
2025-11-19 12:50:41 -0500

    Refactor Firebase initialization and imports

--- firebase.js
+++ firebase.js
@@ -1,8 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js"; 
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+import {getFirestore} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBiHJmw1kFDkmIHcT--Tg3fo2Ynr8EkJUQ",
   authDomain: "crbs-b358b.firebaseapp.com",
@@ -12,6 +17,12 @@ const firebaseConfig = {
   appId: "1:732684413138:web:cf3f37485c55b43da0e50b",
   measurementId: "G-8K0H6MV0JC"
 };
+
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const database = getFirestore(app);
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+
+
+

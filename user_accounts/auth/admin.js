753ce48909ec92115b983f319b9d4918c7a2389b
YahiaAbouelnasr <yhelmy0401@gmail.com>
2025-11-18 19:46:07 -0500

    Implement admin authentication functionality

---
+++ user_accounts/auth/admin.js
@@ -0,0 +1,35 @@
+import {auth, db} from "./firebase.js";
+
+import {signInWithEmailAndPassword}
+from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+
+import {doc, getDoc} 
+from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
+
+const form = document.getElementById("adminForm");
+const emailInput = document.getElementById("adminEmail");
+const passInput = document.getElementById("adminPassword");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const email = emailInput.value;
+    const pass = passInput.value;
+
+    signInWithEmailAndPassword(auth, email, pass) 
+    .then((uCredentials) => {
+        const user = uCredentials.user;
+        return getDoc(doc(db, "users", user.uid))
+        .then((snap)=> {
+            if(!snap.exists() || snap.data().isAdmin !== true) {
+                alert("Access denied, only admins")
+                return;
+            }
+            window.location.href = "../dashboard_notifications/index.html";
+        })
+    })
+    .catch((err)=> {
+        alert(err.message);
+    })
+    
+})

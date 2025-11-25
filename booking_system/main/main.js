95525ddead679bce20e0e472924e52c397651f7f
Bratan911 <matvei.cladcov@gmail.com>
2025-11-24 23:57:33 -0500

    Reformat main.js for improved readability

--- booking_system/main/main.js
+++ booking_system/main/main.js
@@ -1,7 +1,5 @@
-
 import { getResources } from '../shared/shared_data.js';
 import { renderResourcesTable } from '../shared/shared_ui.js';
-// Anastasia what is ../shared/shared_data.js and ../shared/shared_ui.js (again, ali could have deleted it in OLD commit)
 
 const tbody = document.getElementById('resourcesTbody');
 

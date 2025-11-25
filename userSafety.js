cdc87991b11c1e77e12b8e322a9adbcdb47c00ee
YahiaAbouelnasr <yhelmy0401@gmail.com>
2025-11-24 20:44:56 -0500

    Create userSafety.js

---
+++ userSafety.js
@@ -0,0 +1,10 @@
+import {auth} from "./firebase.js";
+import {onAuthStateChanged}
+from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+    if (!user) {
+        window.location.href = "/index.html";
+        return
+    }
+})

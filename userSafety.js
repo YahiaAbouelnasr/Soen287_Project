ec72c152f814b6408b7775e5a518317f062cb668
YahiaAbouelnasr <yhelmy0401@gmail.com>
2025-11-25 00:51:32 -0500

    Add alert for unauthorized access

--- userSafety.js
+++ userSafety.js
@@ -4,6 +4,7 @@ from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
 
 onAuthStateChanged(auth, (user) => {
     if (!user) {
+        alert("Access denied");
         window.location.href = "/index.html";
         return
     }
